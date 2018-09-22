12115f006b4bb235d3454ef1bb7d0390dfd842c2
Dima Efimov <dimaefimov@MacBook-Pro-Dima.local>
2018-09-22 19:02:47 +0300

    5

--- lesson5/5.2.js
+++ lesson5/5.2.js
@@ -22,3 +22,107 @@
   ProjectManager(name,gender,age) => { name, gender, age, type: 'project', rate: '15/h'}
 
 */
+
+
+	const MakeBackendMagic = (obj) => ({
+		makeBackendMagic: () => console.log(obj.name + ' make backend magic')
+	});	 
+
+	const MakeFrontendMagic = (obj) => ({
+		makeFrontendMagic: () => console.log(obj.name + ' make frontend magic')
+	});	  
+
+	const MakeItLooksBeautiful = (obj) => ({
+		makeItLooksBeautiful: () => console.log(obj.name + ' give me colored felt-tip!')
+	});	 
+	
+	const DistributeTasks = (obj) => ({
+		distributeTasks: () => console.log(obj.name + ' tasks for everyone!!!')
+	});	
+
+	const DrinkSomeTea = (obj) => ({
+		drinkSomeTea: () => console.log(obj.name + ' in the world there is not enough chamomile tea')
+	});	
+
+	const WatchYoutube = (obj) => ({
+		watchYoutube: () => console.log(obj.name + ' watch the video')
+	});	
+
+	const Procrastinate = (obj) => ({
+		procrastinate: () => console.log(obj.name + ' everything is almost ready! Just that something does not work = /')
+	});
+
+	// BackendDeveloper constructor
+	const BackendDeveloper = (name, gender, age) => {
+		let particularQualities = {
+			name,
+			gender,
+			age,
+			type: 'backend',
+			rate: '20/h'
+
+		};
+		return Object.assign(
+			{},
+			MakeBackendMagic(particularQualities),
+			DrinkSomeTea(particularQualities),
+			Procrastinate(particularQualities)
+		);
+	};
+
+	// FrontendDeveloper constructor
+	const FrontendDeveloper = (name, gender, age) => {
+		let particularQualities = {
+			name,
+			gender,
+			age,
+			type: 'frontend',
+			rate: '18/h'
+
+		};
+		return Object.assign(
+			{},
+			MakeFrontendMagic(particularQualities),
+			DrinkSomeTea(particularQualities),
+			WatchYoutube(particularQualities)
+		);
+	};
+
+	// Designer constructor
+	const Designer = (name, gender, age) => {
+		let particularQualities = {
+			name,
+			gender,
+			age,
+			type: 'designer',
+			rate: '12/h'
+
+		};
+		return Object.assign(
+			{},
+			MakeItLooksBeautiful(particularQualities),
+			WatchYoutube(particularQualities),
+			Procrastinate(particularQualities)
+		);
+	};
+
+	// ProjectManager constructor
+	const ProjectManager = (name, gender, age) => {
+		let particularQualities = {
+			name,
+			gender,
+			age,
+			type: 'mp',
+			rate: '25/h'
+
+		};
+		return Object.assign(
+			{},
+			DistributeTasks(particularQualities),
+			DrinkSomeTea(particularQualities),
+			Procrastinate(particularQualities)
+		);
+	};
+
+
+
--- lesson5/5.3.js
+++ lesson5/5.3.js
@@ -2,7 +2,7 @@
 
 /*
 
-  Используя методы для создания объектов из задания по композиции написать фабику (HeadHunt), которая будет
+  Используя методы для создания объектов из задания по композиции написать фабрику (HeadHunt), которая будет
   расспределять и создавать сотрудников компании нужного типа.
 
   Данные для списка сотрудников: http://www.json-generator.com/api/json/get/cfeTmcNIXS?indent=2
@@ -16,3 +16,58 @@
   Привязать к интерфейсу и вывести на страницу. На кнопку нанять повесить метод hire из фабрики
 
 */
+const html = document.getElementById("main");
+
+class Persons {
+	show(data) {
+		let personTable = html.querySelector(".person-list")
+		let fragment = document.createDocumentFragment();
+		data.map(pers => {
+			let tr = document.createElement('tr');
+			tr.innerHTML += `<tr>
+				<td><span>${pers.name} (${pers.age})</span></td>
+				<td>${pers.type}</td>
+				<td>
+					<button class="hire" data-id="${pers._id}">Нанять</button><br>
+				</td>
+			</tr>`;
+			fragment.appendChild(tr);
+			let hireBtn = tr.querySelector('.hire');
+
+			hireBtn.addEventListener('click', () => {
+				let emploer = new HeadHunt().hire(pers);
+				emploer.makeFrontendMagic();
+			});
+		});
+		personTable.appendChild(fragment);
+	}
+}
+
+class HeadHunt {
+	hire( obj ){
+		let newEmp;
+		if( obj.type === 'backend'){
+			newEmp = BackendDeveloper(obj.name, obj.gender, obj.age);
+		}
+		if( obj.type === 'frontend'){
+			newEmp = FrontendDeveloper(obj.name, obj.gender, obj.age);
+		}
+		if( obj.type === 'design'){
+			newEmp = Designer(obj.name, obj.gender, obj.age);
+		}
+		if( obj.type === 'project'){
+			newEmp = ProjectManager(obj.name, obj.gender, obj.age);
+		}
+		return newEmp;
+	}
+}
+
+async function getJsonData() {
+  const data = await fetch("http://www.json-generator.com/api/json/get/cfeTmcNIXS?indent=2");
+  return await data.json();
+}	
+
+getJsonData().then( data => {
+	let applicant = new Persons();
+	applicant.show(data);
+});
