3555b02ad0a210104a7efbf787af74536c911078
Dima Efimov <dimaefimov@MacBook-Pro-Dima.local>
2018-09-23 16:46:03 +0300

    7.2

--- lesson7/7.2.js
+++ lesson7/7.2.js
@@ -3,24 +3,60 @@
   Задание:
     1. Используя функциональный декоратор, написать декоратор который будет показывать
        аргументы и результат выполнения функции.
-
-    2. Написать декоратор для класса, который будет преобразовывать аргументы в число,
-       если они переданы строкой, и выводить ошибку если переданая переменная не
-       может быть преобразована в число
 */
 
-const Work1 = () => {
+let doSomethingStrange = (...strangeSymbols) => {
+  	let supperStrange = strangeSymbols.reduce((aught, partOfAught) => {
+      	return aught + partOfAught;
+  	}, 0);
+  	return supperStrange + '$$$';
+}
 
-    class CoolMath {
-      addNumbers(a,b){ return a+b; }
-      multiplyNumbers(a,b){ return a*b}
-      minusNumbers(a,b){ return a-b }
+let decorator = wrapped => {
+  	return function() {
+      	const result = wrapped.apply(this, arguments);
+      	return console.log("Arguments: ", ...arguments, " Result: ", result);
+  	}
+}
+
+const wrapped = decorator(doSomethingStrange);
+
+wrapped('/-/-/', '___', "|-|-");
+
+/*
+2. Написать декоратор для класса, который будет преобразовывать аргументы в число,
+если они переданы строкой, и выводить ошибку если переданая переменная не
+может быть преобразована в число
+*/
+
+
+function checkNumDecorator( target, key, descriptor ) {
+    const originFunction = descriptor.value.bind(this);
+    descriptor.value = function( ...args ) {
+		args.map(item => {
+			if(!isNaN(Number(item))) {
+				console.log(`${item} успешно преобразовано в ${Number(item)}`);
+			}
+			else throw "Невозможно преобразовать к числу";
+		});
+      	return this;
     }
-    let Calcul = new CoolMath();
-    let x = Calcul.addNumbers(2, 2)
-    let y = Calcul.multiplyNumbers(10, 2)
-    let z = Calcul.minusNumbers(10, 2)
-    console.log(x, y, z);
-  };
-  
+}
+
+class CoolMath {
+	@checkNumDecorator;
+	addNumbers(a,b){ return a + b; }
+
+	@checkNumDecorator;
+	multiplyNumbers(a,b){ return a * b}
+	
+	@checkNumDecorator;
+	minusNumbers(a,b){ return a - b }
+}
+const Calcul = new CoolMath();
+let x = Calcul.addNumbers(2, 2)
+let y = Calcul.multiplyNumbers(10, 2)
+let z = Calcul.minusNumbers(10, 2)
+console.log(x, y, z);
+
   
