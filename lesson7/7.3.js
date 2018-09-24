98eebd6e3660f9957ccba3e1d0ff6e153b14acdd
Dima Efimov <dimaefimov@MacBook-Pro-Dima.local>
2018-09-24 19:31:30 +0300

    fix

--- lesson7/7.3.js
+++ lesson7/7.3.js
@@ -22,27 +22,32 @@ const MusicList = [
       {
         id: 1,
         name: 'Du Hast',
-		time: [3, 12]
+		time: [3, 12],
+		parentId: 1
       },
       {
         id: 2,
         name: 'Ich Will',
-        time: [5, 1]
+		time: [5, 1],
+		parentId: 1
       },
       {
         id: 3,
         name: 'Mutter',
-        time: [4, 15]
+		time: [4, 15],
+		parentId: 1
       },
       {
         id: 4,
         name: 'Ich tu dir weh',
-        time: [5, 13]
+		time: [5, 13],
+		parentId: 1
       },
       {
         id: 5,
         name: 'Rammstain',
-        time: [3, 64]
+		time: [3, 64],
+		parentId: 1
       }
     ]
   },
@@ -52,27 +57,32 @@ const MusicList = [
       {
         id: 6,
         name: 'Toxicity',
-        time: [4, 22]
+		time: [4, 22],
+		parentId: 2
       },
       {
         id: 7,
         name: 'Sugar',
-        time: [2, 44]
+		time: [2, 44],
+		parentId: 2
       },
       {
         id: 8,
         name: 'Lonely Day',
-        time: [3, 19]
+		time: [3, 19],
+		parentId: 2
       },
       {
         id: 9,
         name: 'Lost in Hollywood',
-        time: [5, 9]
+		time: [5, 9],
+		parentId: 2
       },
       {
         id: 10,
         name: 'Chop Suey!',
-        time: [2, 57]
+		time: [2, 57],
+		parentId: 2
       }
     ]
   },
@@ -82,22 +92,26 @@ const MusicList = [
       {
         id: 11,
         name: '21 Guns',
-        time: [4, 16]
+		time: [4, 16],
+		parentId: 3
       },
       {
         id: 12,
         name: 'Boulevard of broken dreams!',
-        time: [6, 37]
+		time: [6, 37],
+		parentId: 3
       },
       {
         id: 13,
         name: 'Basket Case!',
-        time: [3, 21]
+		time: [3, 21],
+		parentId: 3
       },
       {
         id: 14,
         name: 'Know Your Enemy',
-        time: [4, 11]
+		time: [4, 11],
+		parentId: 3
       }
     ]
   },
@@ -107,22 +121,26 @@ const MusicList = [
       {
         id: 15,
         name: 'Numb',
-        time: [3, 11]
+		time: [3, 11],
+		parentId: 4
       },
       {
         id: 16,
         name: 'New Divide',
-        time: [4, 41]
+		time: [4, 41],
+		parentId: 4
       },
       {
         id: 17,
         name: 'Breaking the Habit',
-        time: [4, 1]
+		time: [4, 1],
+		parentId: 4
       },
       {
         id: 18,
         name: 'Faint',
-        time: [3, 29]
+		time: [3, 29],
+		parentId: 4
       }
     ]
   }
@@ -132,7 +150,7 @@ function Observable(){
 
   var observers = [];
 
-  this.sendMessage = function( msg ){
+  this.sendMessage = function( ...msg ){
       observers.map( ( obs ) => {
         obs.notify(msg);
       });
@@ -151,13 +169,22 @@ function Observer( behavior ){
 
 let observable = new Observable();
 
-let playListObs = new Observer(id => {
-    let filtredToPlayList = MusicList.filter( item => Number(item.id) === Number(id) );
-    playList.push( filtredToPlayList[0] );
+let playList = [];
+
+let playListObs = new Observer(ids => {
+
+	if(!playList.includes(+ids[0])){
+		let songsArr = MusicList[ids[1] - 1];
+		let filtredToPlayList = songsArr.songs.filter( item => Number(item.id) === Number(ids[0]) );
+		MusicPlayList.innerHTML += `<p>${filtredToPlayList[0].name}</p>`;
+		playList.push( filtredToPlayList[0].id );
+	}
+	else console.log("Уже в плей листе");
 });
 
-let playTrek = new Observer(id => {
-	let filtredToTrek = MusicList.filter( item => Number(item.id) === Number(id) );
+let playTrek = new Observer(ids => {
+	let songsArr = MusicList[ids[1] - 1];
+	let filtredToTrek = songsArr.songs.filter( item => Number(item.id) === Number(ids[0]) );
 	let msg = `Песня ${filtredToTrek[0].name} играет тыц-тыц-тыц... =/`;
     console.log(msg);
 });
@@ -165,22 +192,20 @@ let playTrek = new Observer(id => {
 observable.addObserver( playListObs );
 observable.addObserver( playTrek );
 
-let playList = [];
+
 const MusicBox = () => {
   	const MusicBox = document.getElementById('MusicBox');
   	MusicList.map( Artist => {
 		MusicBox.innerHTML += `<div><h4>${Artist.title}</h4><ul>`;
 		Artist.songs.map( song => {
-			MusicBox.innerHTML += `<li data-song-id="${song.id}">${song.name}</li>`;
+			MusicBox.innerHTML += `<li data-parent-id="${song.parentId}" data-song-id="${song.id}">${song.name}</li>`;
 		});
 		MusicBox.innerHTML += '</ul></div>';
   });
   let songName = MusicBox.querySelectorAll('li');
   songName.forEach(item => {
 	item.onclick = function() {
-		//todo доделать потом =(
-		console.log(this.parentNode);
-		observable.sendMessage(this.dataset.songId);
+		observable.sendMessage(this.dataset.songId, this.dataset.parentId);
 	}
   });
 }
